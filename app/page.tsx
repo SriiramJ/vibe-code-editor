e0217d35da45919fc24fff5efec66509565b111e
SriiramJ <sriram2005j@gmail.com>
2025-10-15 15:59:45 +0530

    03-authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import UserButton from '@/modules/auth/components/user-button';
 
 export default async function Home() {
   return (
@@ -7,6 +8,7 @@ export default async function Home() {
       <Button className="mt-4">
       Get Started!
     </Button>
+    <UserButton/>
     </div>
   );
 }
