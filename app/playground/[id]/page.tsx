6bc621a85a096de900faa440864dfd9a9e1ff1cb
SriiramJ <sriram2005j@gmail.com>
2025-11-01 12:07:45 +0530

    playground file explorer

---
+++ app/playground/[id]/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { useParams } from "next/navigation";
+import React from "react";
+
+const MainPlaygroundPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const {
+    playgroundData,
+    templateData,
+    isLoading,
+    error,
+    loadPlayground,
+    saveTemplateData,
+  } = usePlayground(id);
+  console.log("template data", templateData);
+  console.log("playground data", playgroundData);
+  
+  
+  return <div>Params: {id}</div>;
+};
+
+export default MainPlaygroundPage;
