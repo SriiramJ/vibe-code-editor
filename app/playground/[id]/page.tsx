77e681a5bb6d94b8ba66e6d8bd80974d85cfb936
SriiramJ <sriram2005j@gmail.com>
2025-11-01 13:33:14 +0530

    09-file-explorer-UI

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
 import { usePlayground } from "@/modules/playground/hooks/usePlayground";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -16,8 +20,35 @@ const MainPlaygroundPage = () => {
   console.log("template data", templateData);
   console.log("playground data", playgroundData);
   
-  
-  return <div>Params: {id}</div>;
+  const activeFile = "sample.txt"
+  return (
+  <TooltipProvider>
+  <>
+  <TemplateFileTree
+  data={templateData!}
+          onFileSelect={()=>{}}
+          selectedFile={activeFile}
+          title="File Explorer"
+          onAddFile={()=>{}}
+          onAddFolder={()=>{}}
+          onDeleteFile={()=>{}}
+          onDeleteFolder={()=>{}}
+          onRenameFile={()=>{}}
+          onRenameFolder={()=>{}}
+  />
+  <SidebarInset>
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+      <SidebarTrigger className="-ml-1"/>
+      <Separator className="mr-2 h-4" orientation="vertical"/>
+    </header>
+    <div className= "flex flex-1 items-center gap-2">
+      <div className = "flex flex-col flex-1">
+        <h1  className = "text-sm font-medium">{playgroundData?.title || "Code Playground"}</h1>
+      </div>
+    </div>
+  </SidebarInset>
+  </>
+  </TooltipProvider>)
 };
 
 export default MainPlaygroundPage;
